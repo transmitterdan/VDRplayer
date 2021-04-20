7f5a0c462c2be5826f509909ab3e85107a6b9e65
transmitterdan <10525699+transmitterdan@users.noreply.github.com>
2021-04-20 03:46:26 -0500

    Fix stupid syntax error

--- VDRplayer.py
+++ VDRplayer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
@@ -183,7 +183,7 @@ def tcp(Host, Port, fName, Delay, Repeat):
     if Port is None:
         Port = 2947
     # End if
-    events = false
+    events = False
     f = False
     Server = False
     try:
